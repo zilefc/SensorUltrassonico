9ebbc1b42a6ad23e495c616aa7b781ebeb14d213
Frederico Zile <46302322+zilefc@users.noreply.github.com>
2020-07-17 20:21:21 +0300

    Code and lib

---
+++ SensorUltrassonico.ino
@@ -0,0 +1,19 @@
+/*
+ * Frederico Zile
+ * fczile@gmail.com
+ */
+#include <Ultrasonic.h>
+ Ultrasonic ultrassom(8,7); // passa (trigger,echo)
+
+ void setup() {
+ Serial.begin(9600); 
+ 
+ }
+ligada
+ void loop()
+ {
+  float medida = ultrassom.Ranging(CM);
+   Serial.print(medida);
+   Serial.println("cm");
+   delay(150);
+ } 
